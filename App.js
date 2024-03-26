1781591a2f3512bccc6f3ef849f82ce0a4acdb95
Kauê Evangelista da Silva Aguiar <kauesilva38@hotmail.com>
2024-03-26 12:00:49 -0300

    comit

--- App.js
+++ App.js
@@ -16,8 +16,6 @@ import * as ImagePicker from "expo-image-picker";
 import * as MediaLibrary from "expo-media-library";
 
 //geolocalização
-import MapView, { Marker } from "react-native-maps";
-import * as Location from "expo-location";
 import GeoLocalizacao from "./src/Components/GeoLocalizacao";
 
 export default function App() {
@@ -75,66 +73,6 @@ export default function App() {
   // ===========//
 
   /* Fim captura de foto */
-
-  /* Geolocalização */
-
-  /* State para monitorar dados da atulização atual do usuário inicialmente, nulo */
-  const [minhaLocalizacao, setMinhaLocalizacao] = useState(null);
-
-  //state para condicional caso o botão ver localização for pressionado
-  const [botaoLocalizacao, setBotaoLocalizacao] = useState(false);
-
-  //state para quando o botao salvar for acionado
-  const [botaoSalvar, setBotaoSalvar] = useState(false);
-
-  useEffect(() => {
-    async function obterLocalizacao() {
-      /* ACessando o status da requisição de permissão de uso dos recursos de geolocalização */
-      const { status } = await Location.requestForegroundPermissionsAsync();
-
-      /* Se o status não for liberado/permitido, então será dado um alerta notificando o usuário. */
-      if (status !== "granted") {
-        Alert.alert("Ops!", "Você não autorizou o use do geolocalização");
-        return;
-      }
-
-      /* Se o status estiver ok, obtemos os dados de localização */
-      let localizacaoAtual = await Location.getCurrentPositionAsync({});
-      setMinhaLocalizacao(localizacaoAtual);
-    }
-
-    obterLocalizacao();
-  }, []);
-
-  console.log(minhaLocalizacao);
-
-  //este state tem a finalidade de determinar a posição no mapview junto com o Marker. Inicialmente é nulo pois o usuário ainda não acionou o botão da sua localização
-  const [localizacao, setLocalizacao] = useState(null);
-
-  //Coordenadaspara o MapView
-  const regiaoInicialMapa = {
-    //São Paulo
-    latitude: -23.533773,
-    longitude: -46.65529,
-
-    latitudeDelta: 40,
-    longitudeDelta: 40,
-  };
-
-  //nessa função estamos pegando os dados da latitude e longitude dentro do usestate e alterando para a latitude/longitude de onde a pessoa clicar/selecionar no mapa
-  const marcarLocal = (event) => {
-    setLocalizacao({
-      //obtendo novos valores a partir da geolocalização da posição do usuário
-      latitude: minhaLocalizacao.coords.latitude,
-      longitude: minhaLocalizacao.coords.longitude,
-      latitudeDelta: 0.02,
-      longitudeDelta: 0.01,
-    });
-
-    setBotaoLocalizacao(true);
-  };
-
-  /* Fim Geolocalização */
 
   /* Salvar foto com titulo e localização */
   const salvarFoto = () => {
---
+++ src/Components/Camera.js
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  ScrollView,
+  Pressable,
+} from "react-native";
+
+//captura de imagens
+import * as ImagePicker from "expo-image-picker";
+import * as MediaLibrary from "expo-media-library";
+
+export default function Camera() {
+  /* Captura de foto */
+  const [foto, setFoto] = useState(null);
+  const [status, requestPermission] = ImagePicker.useCameraPermissions();
+
+  const [titulo, setTitulo] = useState("");
+
+  //permissao para acessara câmera
+
+  useEffect(() => {
+    async function verificarPermissao() {
+      const cameraStatus = await ImagePicker.requestCameraPermissionsAsync();
+      requestPermission(cameraStatus === "granted");
+    }
+    verificarPermissao();
+  }, []);
+  /* =============== */
+
+  /* abrir a câmera  */
+  const abrirCamera = async () => {
+    const imagem = await ImagePicker.launchCameraAsync({
+      allowsEditing: false,
+      aspect: [16, 9],
+      quality: 1,
+    });
+    /* =========== */
+
+    /* salvando imagem na memória física */
+    if (!imagem.canceled) {
+      //Usando API do MediaLibrary para salvar no armazenamento físico do dispositivo
+      await MediaLibrary.saveToLibraryAsync(imagem.assets[0].uri);
+      setFoto(imagem.assets[0].uri);
+    }
+  };
+  /* ============= */
+
+  //Ao pressionar o botão, executa esta função
+  const escolherFoto = async () => {
+    //Acessando via ImagePicker a biblioteca para a seleção de apenas imagens, com recursos de edição habilitado, proporção de 16,9 e qualidade total.
+    const resultado = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: false,
+      aspect: [16, 9],
+      quality: 1,
+    });
+
+    if (!resultado.canceled) {
+      setFoto(resultado.assets[0].uri);
+    }
+  };
+  console.log(foto);
+
+  // ===========//
+
+  /* Fim captura de foto */
+
+  /* Salvar foto com titulo e localização */
+  const salvarFoto = () => {
+    // salvar a foto junto com o título e a localização
+    const fotoSalva = {
+      uri: foto,
+      titulo: titulo,
+      localizacao: localizacao,
+    };
+    setBotaoSalvar(true);
+  };
+}
